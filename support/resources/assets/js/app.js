e10e048f825c830a6c8504e53ff6fa8e907ba312
huynhminhnhan <huynhminhnhan69@gmail.com>
2019-09-09 08:03:26 +0700

    add bootstrap vue and routing

--- support/resources/assets/js/app.js
+++ support/resources/assets/js/app.js
@@ -9,7 +9,9 @@ require('./bootstrap');
 window.Vue = require('vue');
 import VueRouter from 'vue-router'
 import { routes }  from './router/index';
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue)
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
--- support/webpack.mix.js
+++ support/webpack.mix.js
@@ -18,16 +18,11 @@ mix.js('resources/assets/js/app.js', 'public/js')
         'public/assets/vendors/iconfonts/ionicons/css/ionicons.css',
         'public/assets/vendors/iconfonts/typicons/src/font/typicons.css',
         'public/assets/vendors/iconfonts/flag-icon-css/css/flag-icon.min.css',
-        'public/assets/vendors/css/vendor.bundle.base.css',
-        'public/assets/vendors/css/vendor.bundle.addons.css',
         'public/assets/css/shared/style.css',
         'public/assets/css/demo_1/style.css',
     ],'public/admin/css/library.min.css')
     .copyDirectory('public/assets/images','public/images')
     .scripts([
-        'public/assets/vendors/js/vendor.bundle.base.js',
-        'public/assets/vendors/js/vendor.bundle.addons.js',
-        'public/assets/js/shared/off-canvas.js',
         'public/assets/js/shared/misc.js',
         'public/assets/js/demo_1/dashboard.js'
     ],'public/admin/js/library.min.js').version();
