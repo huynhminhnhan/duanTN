31f6179d425750bf204eac094ced0cb1cc46412a
huynhminhnhan <huynhminhnhan69@gmail.com>
2019-09-30 09:18:05 +0700

    fix seed erro

--- support/database/seeds/DatabaseSeeder.php
+++ support/database/seeds/DatabaseSeeder.php
@@ -11,14 +11,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(DepartmentTableSeeder::class);
+        
         // User seeder will use the roles above created.
-        //$this->call(RoleTableSeeder::class);
+        $this->call(RoleTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(DepartmentTableSeeder::class);
    	 // User seeder will use the roles above created.
-      // $this->call(AccountsTableSeeder::class);
-      //  $this->call(UserTableSeeder::class);
-        //$this->call(ChamcongSeeder::class);
-       // $this->call(QuestionSeeder::class);
+       $this->call(AccountsTableSeeder::class);
+       
+        $this->call(ChamcongSeeder::class);
+        $this->call(QuestionSeeder::class);
         
     //    khi nao tạo seed mới thì đóng các class đã tạo seed rồi lại
     }
