020004c2c0f156fcf342758c2289be684589a4bb
HuynhMinhNhan <huynhminhnhan69@gmail.com>
2019-10-16 19:26:02 +0700

    hêhhe

--- support/app/Mail/sendmailDepart.php
+++ support/app/Mail/sendmailDepart.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class OrderShipped extends Mailable
+class sendmailDepart extends Mailable
 {
     public $content;
     public $subject;
